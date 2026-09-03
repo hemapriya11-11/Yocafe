bd86ecd586643b972fe78f2361d93429dbc4d89e
Hemapriya R <hemapriyaa.984@gmail.com>
2026-09-03 16:59:14 +0530

    feat: add health check endpoint

--- src/server.js
+++ src/server.js
@@ -21,6 +21,10 @@ app.use(
 app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
 
+app.get("/health", (req, res) => {
+  return res.status(200).send("OK");
+});
+
 app.use("/auth", authRouter);
 
 app.use(errorHandler);
